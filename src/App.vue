770e63a35be7dbc81934e9d3606afe99de59a0f6
Philip <Philip@chxian.com>
2022-04-02 18:17:34 +0800

    init code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script setup>
+import { ref,shallowRef } from 'vue'
+const foo = shallowRef({fooProp:1})
+const bar = ref(1);
+let nonReactive = 1
+function onClick(){
+  // #foo is updated
+  foo.value.fooProp++;
+  bar.value++;
+  // when nonReactive changes, #nonReactive still has the old value 1
+  // but in SFC playground, it will be updated to 2
+  nonReactive++;
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id='foo'>
+    {{foo.fooProp}}
+  </div>
+  <div id='bar'>
+    {{bar}}
+  </div>
+  <div id='nonReactive'>
+    {{nonReactive}}
+  </div>
+  <button @click='onClick'>
+    clickme!
+  </button>
+</template>
